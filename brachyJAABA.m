5c75e98d27f4df5ac97a7d60448e769a037d4c21
Eric Fortune <eric.fortune@njit.edu>
2018-08-02 10:10:15 -0400

    / ‘brachyJAABA.m’

--- brachyJAABA.m
+++ brachyJAABA.m
@@ -243,7 +243,7 @@ tim = 1/dFs:1/dFs:length(fpulses)/dFs;
 % If a single fish
 if length(in) == 1
     figure(2); clf;
-    subplot(211); plot(tim, abs(dfpulses)); xlim([0 1]);
+    subplot(211); plot(tim, abs(dfpulses)); xlim([0 0.5]);
     tmp = singletrackEOD(eodata.values, length(im), 1/eodata.interval);
     out.eodfreq = tmp.freq;
     out.eodtim = tmp.tim;
