1f4a605ca7d4837c847a5167dfff64cce9c29b89
Eric Fortune <eric.fortune@njit.edu>
2018-08-02 11:58:45 -0400

    / ‘brachyJAABA.m’

--- brachyJAABA.m
+++ brachyJAABA.m
@@ -254,7 +254,7 @@ tim = 1/dFs:1/dFs:length(fpulses)/dFs;
         % Find next peak above threshold
         nextim = tim(find(abs(dfpulses(tim > currtim)) > curramp * 0.75, 1)) + currtim;
         [curramp, curridx] = max(abs(dfpulses(tim > nextim & tim < nextim+0.010)));
-        toe(end+1) = tim(curridx + length(find(tim < nextim)))
+        toe(end+1) = tim(curridx + length(find(tim < nextim)));
         amp(end+1) = curramp;
         currtim = toe(end)
     end
