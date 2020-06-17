ca81f9e633b3ee8cf07c3b5c7fb71203bc28c270
Eric Fortune <eric.fortune@njit.edu>
2020-06-17 11:09:17 -0400

    / ‘ritaclick.m’

--- ritaclick.m
+++ ritaclick.m
@@ -7,7 +7,7 @@ function out = ritaclick(in)
 
 numframes = in.FrameRate * in.Duration; % Calculate total frames in video
 
-fprintf('There are $i frames./n', numframes);
+fprintf('There are %i frames./n', numframes);
 
 figure(1);
 
@@ -19,16 +19,21 @@ for j=2:2:numframes % for the entire video
    clf; imshow(currframe); hold on; % Clear the figure, show the frame
 
    [out.shuttlex(j/2), out.shuttley(j/2)] = ginput(1); % User click the shuttle
-        plot(out.shuttlex(j/2), out.shuttley(j/2), '.m', 'MarkerSize', 8); drawnow;
+        plot(out.shuttlex(j/2), out.shuttley(j/2), '.m', 'MarkerSize', 12); drawnow;
 
         [out.fishx(j/2), out.fishy(j/2)] = ginput(1); % User click the fish
-        plot(out.fishx(j/2), out.fishy(j/2), '.g', 'MarkerSize', 8); drawnow;
+        plot(out.fishx(j/2), out.fishy(j/2), '.g', 'MarkerSize', 12); drawnow;
         
    out.tim(j/2) = (1/in.FrameRate) * j; % Add the time of the current frame
 
    pause(0.2); % Allows the user to see the second click.  Could be 0.1 for faster work.
 
-if remai   
+    if rem(j / 20) == 0
+
+       fprintf('Current Frame is %i./n', j);
+       save temp.mat out
+
+    end
    
 end
 
