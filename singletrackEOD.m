00834e3f4c244a61caa95b79f45a1a997ef9f462
eric_fortune <eric.fortune@njit.edu>
2021-03-11 13:04:45 -0500

    / ‘singletrackEOD.m’

--- singletrackEOD.m
+++ singletrackEOD.m
@@ -18,11 +18,21 @@ etim = 1/Fs:1/Fs:length(data)/Fs;
 
     vtims = videotimes; % times, not length !!!
 
-tmp = fftmachine(data, Fs); 
-[~, idx] = max(tmp.fftdata);
-precisefreq = tmp.fftfreq(idx);
-rango = [precisefreq-de, precisefreq+de];
+    % Get target frequency based on max amplitude
+%     tmp = fftmachine(data, Fs); 
+%     [~, idx] = max(tmp.fftdata);
+%     precisefreq = tmp.fftfreq(idx);
+%     rango = [precisefreq-de, precisefreq+de];
 
+    % Get target frequency based on user click
+    figure(27); clf; 
+    freqres = 2; % multiple of 2 between 2 and 32
+    specgram(data, 1024*freqres, Fs, [], round(1024*freqres*0.85));
+    ylim([200 1100]); colormap('HOT'); caxis([-20 30]);
+    fprintf('Click on the EOD frequency.\n');
+    [~, precisefreq] = ginput(1);
+    rango = [precisefreq-de, precisefreq+de];
+    
 % Get frequencies
 
 % % Single processor way
@@ -54,7 +64,7 @@ end
 %% Plot the data to make the user happy - comment this out if you don't need happiness
 figure(1); clf; 
     ax(1) = subplot(211);
-    specgram(data, 4096, Fs, [], 4000); ylim([300 1000]); caxis([-10 30]); colormap('HOT');
+    specgram(data, 4096, Fs, [], 4000); ylim([100 1000]); caxis([-10 30]); colormap('HOT');
     hold on;
     plot(out.tim, out.freq, 'g.', 'MarkerSize', 2);
     ax(2) = subplot(212);
